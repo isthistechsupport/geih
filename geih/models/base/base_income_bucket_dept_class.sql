82dddeb8e15bf12eee558325058fae7d66918ba9
isthistechsupport <32936232+isthistechsupport@users.noreply.github.com>
2023-05-21 12:50:20 -0500

    added pop buckets for income

---
+++ geih/models/base/base_income_bucket_dept_class.sql
@@ -0,0 +1,12 @@
+select
+    sum(expf) over (partition by department, class order by income) - expf as bucket_bottom,
+    sum(expf) over (partition by department, class order by income) as bucket_top,
+    expf,
+    department,
+    class,
+    income
+from {{ ref('base_income_dept_class') }}
+order by
+    income,
+    department,
+    class;
